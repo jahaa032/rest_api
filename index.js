868608f93ea7def9ff817ba8d895a7eead0a81b5
thewiz <jfhannestad@gmail.com>
2026-02-12 09:42:22 +0100

    ERror img

--- index.js
+++ index.js
@@ -11,20 +11,23 @@
 
 //     .then(data => console.log(data.name))
 //     .catch(error => consol.error(error));
-async function fetchData(){
-    try{
-        
-        const pokemonName = document.getElementById("pokemonName").ariaValueMax.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new error("Could not fetch resource")
         }
 
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprite.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
-    catch(error){
+    catch (error) {
         console.error(error);
     }
 }
