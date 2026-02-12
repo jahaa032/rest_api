8952ac227e79cfe484a462bfae94ec70637c12a7
thewiz <jfhannestad@gmail.com>
2026-02-12 09:18:25 +0100

    Added async and await function + some html elements

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+// const url = "https://pokeapi.co/api/v2/pokemon/arceus"
 
+// fetch(url)
+//     .then(response =>  {
 
-fetch("https://pokeapi.co/api/v2/pokemon/arceus")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => consol.error(error));
+//         if (!response.ok){
+//             throw new error("Could not fetch resource");
+//         }
+//         return response.json();
+//     })
+
+//     .then(data => console.log(data.name))
+//     .catch(error => consol.error(error));
+async function fetchData(){
+    try{
+        
+        const pokemonName = document.getElementById("pokemonName").ariaValueMax.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+        if(!response.ok){
+            throw new error("Could not fetch resource")
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
