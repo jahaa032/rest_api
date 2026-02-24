af3c561ecdb2d01b9df8c59d4cabde02ef6b89e0
thewiz <jfhannestad@gmail.com>
2026-02-24 09:53:08 +0100

    something

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-// const url = "https://pokeapi.co/api/v2/pokemon/arceus"
-
-// fetch(url)
-//     .then(response =>  {
-
-//         if (!response.ok){
-//             throw new error("Could not fetch resource");
-//         }
-//         return response.json();
-//     })
-
-//     .then(data => console.log(data.name))
-//     .catch(error => consol.error(error));
 async function fetchData() {
     try {
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if (!response.ok) {
-            throw new error("Could not fetch resource")
+            throw new Error("Could not fetch resource")
         }
 
         const data = await response.json();
-        const pokemonSprite = data.sprite.front_default;
+        console.log(data);
+
+        const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
 
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+
+       
+
+        .then(data => {
+            data.forEach(pokemon => {
+                const markup = `<li>${pokemon.types}</li>`;
+
+                document.querySelector('ul').insertAdjacentHTML('beforeend', markup)
+            })
+        })
     }
     catch (error) {
         console.error(error);
